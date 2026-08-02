7890ace2bd8b3c74dad68943ea10f30e1300ee02
Tomislav Dukez <tomdu3@ymail.com>
2026-08-02 21:13:39 +0100

    fix redo displayCard to use template

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 function setup() {
   const allEpisodes = getAllEpisodes();
   const oneEpisode = getOneEpisode();
-  makePageForEpisodes(allEpisodes);
 }
 
 function makePageForEpisodes(episodeList) {
@@ -11,33 +10,18 @@ function makePageForEpisodes(episodeList) {
 }
 
 function displayEpisodeCard(episode) {
-  const cardEpisode = document.createElement("section");
-  cardEpisode.classList.add("card", "episode");
-  const title = document.createElement("h2");
-  title.classList.add("title", "episode");
-  const episodeCode = document.createElement("p");
-  episodeCode.classList.add("code", "episode");
-  const image = document.createElement("img");
-  image.classList.add("image", "episode");
-  const summary = document.createElement("p");
-  summary.classList.add("summary", "episode");
-
-  title.innerText = episode.name;
-  episodeCode.innerText = `S${String(episode.season).padStart(2, "0")}E${String(episode.number).padStart(2, "0")}`;
-  image.src = episode.image.medium;
+  const card = document.getElementById("episode-card").content.cloneNode(true);
+  card.querySelector("h3").innerText =
+    `${episode.name} - S${String(episode.season).padStart(2, "0")}E${String(episode.number).padStart(2, "0")}`;
+  card.querySelector("img").src = episode.image.medium;
   // use regex to remove tags from summary  value in db
-  summary.innerText = episode.summary.replace(/<[^>]*>/g, "");
-
-  cardEpisode.appendChild(title);
-  cardEpisode.appendChild(episodeCode);
-  cardEpisode.appendChild(image);
-  cardEpisode.appendChild(summary);
-  return cardEpisode;
+  card.querySelector("p").innerText = episode.summary.replace(/<[^>]*>/g, "");
+  return card;
 }
 
 // test for one episode
-const details = document.getElementById("details");
+const rootElem = document.getElementById("root");
 const episodeEl = displayEpisodeCard(getOneEpisode());
-details.appendChild(episodeEl);
+root.appendChild(episodeEl);
 
 window.onload = setup;
